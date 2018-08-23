323ee9ae8595204284392411e03ce5e9bd58934c
Iryna Ieremeichuk <ieremeichukiryna@gmail.com>
2018-08-23 12:42:17 -0700

    other changes to Application and Group classes

--- fixtures/application.py
+++ fixtures/application.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixtures.session import SessionHelper
+from fixtures.group_helper import GroupHelper
 
 
 class Application:
@@ -8,6 +9,7 @@ class Application:
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         self.wd.get("http://localhost/addressbook")
--- pages/group.py
+++ pages/group.py
@@ -9,32 +9,3 @@ class Group:
         self.name = name
         self.header = header
         self.footer = footer
-
-    groups = driver.find_element_by_link_text("groups")
-    group_page = driver.find_element_by_link_text("group page")
-    new_group_button = driver.find_element_by_name("new")
-    group_name = driver.find_element_by_name("group_name")
-    group_header = driver.find_element_by_name("group_header")
-    group_footer = driver.find_element_by_name("group_footer")
-    submit_button = driver.find_element_by_name("submit")
-
-    def go_to_groups_page(self):
-        self.groups.click()
-
-    def return_to_groups_page(self):
-        self.group_page.click()
-
-    def create_group(self):
-        self.go_to_groups_page()
-        self.new_group_button.click()
-        self.group_name.click()
-        self.group_name.click().clear()
-        self.group_name.click().send_keys(self.name)
-        self.group_header.click()
-        self.group_header.clear()
-        self.group_header.send_keys(self.header)
-        self. group_footer.click()
-        self.group_footer.clear()
-        self.group_footer.send_keys(self.footer)
-        self.submit_button.click()
-        self.return_to_groups_page()
